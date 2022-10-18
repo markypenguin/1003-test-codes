fb0e0226d6de3d2216ec34ff9500d6673e178481
markypenguin <markpengjz@gmail.com>
2022-10-18 23:27:57 +0800

    update exp

--- exponential.c
+++ exponential.c
@@ -3,8 +3,26 @@
 
 int main()
 {
-    double x, limit = 1 * pow(10, -6), absx;
-    int n;
-    // testing does this work?
+    double x, absx, limit = 1 * pow(10, -6), term = 1.0, sum = 1.0, expans, diff;
+    int i, n = 1;
 
+    printf("Input value of x: ");
+    scanf("%lf", &x);
+    absx = x<0? -x:x;
+
+    do
+    {
+        term *= (absx / n);
+        sum += term;
+        printf("\n term = %le    sum = %le     n term is %d", term, sum, n);
+        n += 1;
+    }
+
+     while (term >= limit);
+
+     sum = x<0? (1.0/sum):sum;
+     expans = exp(x);
+     diff = expans - sum;
+     printf("\n C Lib Function exp(x) = %le       My code sum = %le", expans, sum);
+     printf("\n\nThe error bewteen infinite series and math lib func is %le\n\n", diff);
 }
