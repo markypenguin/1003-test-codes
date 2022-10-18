0bc65489849971fa2d231f3c82df136501856fc8
markypenguin <markpengjz@gmail.com>
2022-10-19 00:43:29 +0800

    update

---
+++ functiontest.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int func (int m);
+
+int main()
+{
+    int x, funcx;
+    printf(" Random number input: ");
+    scanf("%d", &x);
+    printf("\n Value of x = %d", x);
+    funcx = func(x);
+    printf("\n funcx = %d", funcx);
+    return 0;
+}
+
+int func(int m)
+{
+    if (m == 0)
+    {
+        printf("\n value of m = %d", m);
+        return (1);
+    }
+    else
+    {
+        printf("\n Value of !m = %d", m);
+        return(m * func(m - 1));
+    }
+}
+
+
---
+++ series.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int x_plus_y()
+{
+
+}
+
+
+int binomial()
+{
+
+}
+
+int main()
+{
+    double x, y, result;
+    int n;
+    printf("\nInput value x: ");
+    scanf("%lf", x);
+    printf("\nInput value y: ");
+    scanf("%lf", y);
+    printf("\nInput value n: ");
+    scanf("%f", n);
+}
